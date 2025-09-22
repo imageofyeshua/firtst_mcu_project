fad2a3ca828b03724c030130656849365f5f248d
Daniel Park <intheimageofyeshua@gmail.com>
2025-09-22 13:57:52 +0900

    Align

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@
 
 use core::panic::PanicInfo;
 
+mod startup_stm32f103;
+
+/* global array */
+static mut SCORES_GLOBAL: [i32; 5] = [1, 2, 3, 4, 5];
+const NUMBERS: [i32; 5] = [1, 2, 3, 4, 5];
+static mut BUFFER: [u8; 1024] = [0; 1024];
+
 #[unsafe(no_mangle)]
 fn main() {
-    
+
+    let mut total_score = 0;
+
+    for score in SCORES_GLOBAL {
+        total_score += score;
+    }
+
     loop {
 
     }
--- src/startup_stm32f103.rs
+++ src/startup_stm32f103.rs
@@ -3,12 +3,10 @@
 
 fn reset_handler() {
     /* copy the .data section from FLASH to RAM */
-
-
     /* zero out the .bss section in the RAM */
-
-
     /* call main() */
+
+    create::main();
 }
 
 
