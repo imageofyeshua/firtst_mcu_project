50079f071c741d10cbb3d43ca10f0a19a113dd9d
Daniel Park <intheimageofyeshua@gmail.com>
2025-09-27 11:53:01 +0900

    Update on Clear and Set Register Function

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,24 @@
 #![allow(unused_variables, dead_code)]
 
 use core::panic::PanicInfo;
+use led::*;
+use button::*;
+
+use crate::board::{BLUE_LED_PIN, BLUE_LED_PORT};
 
 mod startup_stm32f103;
-
-/* global array */
-static mut SCORES_GLOBAL: [i32; 5] = [1, 2, 3, 4, 5];
-const NUMBERS: [i32; 5] = [1, 2, 3, 4, 5];
-static mut BUFFER: [u8; 1024] = [0; 1024];
+mod led;
+mod button;
+mod mcu;
+mod board;
 
 #[unsafe(no_mangle)]
 fn main() {
 
-    let mut total_score = 0;
-
-    unsafe {
-        for score in SCORES_GLOBAL {
-            total_score += score;
-        }
-    }
-
-    unsafe {
-        BUFFER[0] = 100;
-    }
+    led_init(BLUE_LED_PORT, BLUE_LED_PIN);
+    led_on(BLUE_LED_PORT, BLUE_LED_PIN);
+    //button_init(BUTTON_PIN);
+    //button_configure_interrupt(BUTTON_PIN);
 
     loop {
 
@@ -37,4 +33,9 @@ fn panic_handler(_info: &PanicInfo) -> ! {
     loop {
 
     }
+}
+
+// button interrupt handler
+fn EXTIO_Handler() {
+    led_toggle(BLUE_LED_PORT, BLUE_LED_PIN);
 }
